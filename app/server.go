6e6420d639fe891384c206ed1b1148383f7fc214
baselrabia <baselrabia@gmail.com>
2024-04-08 08:44:15 +0200

    pass 3nd stage

--- app/server.go
+++ app/server.go
@@ -12,13 +12,13 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	
+
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
+
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
@@ -26,9 +26,31 @@ func main() {
 	}
 
 	defer conn.Close()
-	response := []byte("HTTP/1.1 200 OK\r\n\r\n")
-	if _, err = conn.Write(response); err != nil {
+	handleConn(conn)
+}
+func handleConn(conn net.Conn) {
+	reqStr := readConnToString(conn)
+	fmt.Println(reqStr)
+	httpReq := newHttpReq(reqStr)
+	responder(conn, httpReq)
+}
+func readConnToString(conn net.Conn) string {
+	buff := make([]byte, 1024)
+	_, err := conn.Read(buff)
+	if err != nil {
+		fmt.Println("Error reading connection: ", err.Error())
+		os.Exit(1)
+	}
+	return string(buff)
+}
+func responder(conn net.Conn, req *httpReq) {
+	response := NotFoundResponse
+	if req.path == "/" {
+		response = OkResponse
+	}
+	if _, err := conn.Write([]byte(response)); err != nil {
 		fmt.Println("Error writing into connection: ", err.Error())
 		os.Exit(1)
+
 	}
 }
